7041b630227fcf34cbd3a35207dfbbec7f13ecd3
Cesar Valdez <cesar.valdez@feedhenry.com>
2016-08-26 10:10:21 +0300

    promisified CRUD for user

--- application.js
+++ application.js
@@ -9,9 +9,9 @@ var DefaultRouter = require('./lib/utils/routers/default');
 var ErrorHandler = require('./lib/utils/errors/util');
 var ErrStrategies = require('./lib/utils/errors/basic');
 
-var MONGO_URL = process.env.MONGO_URL || 'mongodb://@192.168.177.149:27018/sampledb';
+var MONGO_URL = process.env.MONGO_URL || 'mongodb://@10.43.2.243:27018/test';
 var PORT = process.env.FH_PORT || process.env.OPENSHIFT_NODEJS_PORT || 8001;
-var HOST = process.env.NODEJS_IP || '127.0.0.1';
+var HOST = process.env.NODEJS_IP || '0.0.0.0';
 
 var db = new DBConnection(MONGO_URL);
 
@@ -21,9 +21,8 @@ var errorHandler = ErrorHandler([ErrStrategies.basic,
 
 var app = express();
 
-
-app.use('/user', require('./lib/routes/user')(DefaultRouter(), db));
-
+app.get('/', (req,res)=>{ res.send('services deployed') });
+app.use('/user', require('./lib/routes/restful')(DefaultRouter(), db.use('user')));
 
 // error handling middleware.
 errorHandler(app);
--- test/integration/integration.js
+++ test/integration/integration.js
@@ -3,50 +3,67 @@ var expect = require('chai').expect
 
 const URL = process.env.INTEGRATION_HOST || 'http://0.0.0.0:8001';
 
-describe('Integration Testing for ' + URL, function(){
-  var id
+describe('Integration Testing for ' + URL, function() {
+    var id
 
-  console.log('Testing: ' + URL);
+    console.log('Testing: ' + URL);
 
-  it('post object', function(done){
-    superagent.post(URL +'/user')
-      .send({ name: 'John'
-        , email: 'john@rpjs.co'
-      })
-      .end(function(e,res){
-      //  console.log(res.body)
-        var results = res.body.ops;
-        expect(e).to.eql(null)
-        expect(results.length).to.eql(1)
-        expect(results[0]._id.length).to.eql(24)
-        id = results[0]._id
-        done()
-      })
-  })
+    it('post object', function(done) {
+        superagent.post(URL + '/user')
+            .send({
+                name: 'John',
+                email: 'john@rpjs.co'
+            })
+            .end(function(e, res) {
+                //  console.log(res.body)
+                var results = res.body.ops;
+                expect(e).to.eql(null)
+                expect(results.length).to.eql(1)
+                expect(results[0]._id.length).to.eql(24)
+                id = results[0]._id
+                done()
+            })
+    })
 
-  it('retrieves an object', function(done){
-    superagent.get(URL +'/user/'+id)
-      .end(function(e, res){
+    it('retrieves an object', function(done) {
+        superagent.get(URL + '/user/' + id)
+            .end(function(e, res) {
 
-      //  console.log(res.body)
+                //console.log(res.body)
 
-        expect(e).to.eql(null)
-        expect(res.body.response._id).to.be.equal(id);
-        done()
-      })
-  })
+                expect(e).to.eql(null)
+                expect(res.body._id).to.be.equal(id);
+                done()
+            })
+    })
 
-  it('retrieves a collection', function(done){
-    superagent.get(URL +'/user')
-      .end(function(e, res){
-        //console.log(res.body)
-        var results = res.body.response;
+    it('retrieves a collection', function(done) {
+        superagent.get(URL + '/user')
+            .end(function(e, res) {
+                //console.log(res.body)
+                //console.log(e)
+                var results = res.body;
 
-        expect(e).to.eql(null)
-        expect(results.length).to.be.above(0)
-        expect(results.map(function (item){return item._id})).to.contain(id)
-        done()
-      })
-  })
+                expect(e).to.eql(null)
+                expect(results.length).to.be.above(0)
+                expect(results.map(function(item) {
+                    return item._id
+                })).to.contain(id)
+                done()
+            })
+    })
+
+
+    it('removes an object', function(done) {
+        superagent.del(URL + '/user/' + id)
+            .end(function(e, res) {
+                 //console.log(res.body)
+                expect(e).to.eql(null)
+                expect(res.body).to.eql(true)
+                done()
+            })
+    })
+
 
 })
+
