9da93e802ddc5b0eb2402dd37e6b0bbfbd843ec3
Cesar Valdez <cesar.valdez@feedhenry.com>
2016-08-31 11:35:26 +0300

    changing the port to a fixed one, no need to left that thing configurable when using containers

--- Readme.md
+++ Readme.md
@@ -1,6 +1,8 @@
-# Basic Dockerized Node.js service.
+# Basic Openshift 3.2 Node.js microservice.
 
 ## Node.js basic scaffolding
+
+- node@v6.2.0 
 
 - Mocha framework
  - Chai.js assertions library.
@@ -12,7 +14,18 @@
 - BodyParser
 - [Mongoskin](https://github.com/kissjs/node-mongoskin)
 
+
+## How to run this
+
+
+```sh
+oc login https://10.2.2.2:8443   
+oc create -f openshift/node-mongo.json
+```
+
+
 ## Relevant links
+
 
 - Docker
   - [Docker and Node.js Best Practices](https://github.com/nodejs/docker-node/blob/master/docs/BestPractices.md)
--- application.js
+++ application.js
@@ -9,12 +9,11 @@ var DefaultRouter = require('./lib/utils/routers/default');
 var ErrorHandler = require('./lib/utils/errors/util');
 var ErrStrategies = require('./lib/utils/errors/basic');
 
-var MONGO_URL = process.env.MONGO_URL || 'mongodb://@10.43.2.243:27018/test';
-var PORT = process.env.FH_PORT || process.env.OPENSHIFT_NODEJS_PORT || 8001;
-var HOST = process.env.NODEJS_IP || '0.0.0.0';
+const MONGO_URL = process.env.MONGO_URL || 'mongodb://@10.43.2.243:27018/test';
+const PORT = 8080;
+const HOST = '0.0.0.0';
 
 var db = new DBConnection(MONGO_URL);
-
 
 var errorHandler = ErrorHandler([ErrStrategies.basic,
                                  ErrStrategies.log]);
@@ -23,6 +22,7 @@ var app = express();
 
 app.get('/', (req,res)=>{ res.send('services deployed') });
 app.use('/user', require('./lib/routes/restful_decorator').routing(DefaultRouter(), db.use('user')));
+
 
 // error handling middleware.
 errorHandler(app);
