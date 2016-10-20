182d3773b199eae777dea1ff1e9ba268d2039b74
Cesar <cesarv01@gmail.com>
2016-10-20 14:22:43 +0000

    improvemnets

--- application.js
+++ application.js
@@ -1,17 +1,17 @@
 'use strict';
-// version 0.1
 
-var express = require('express');
+// version 0.1
+let express = require('express');
 
 //db
-var mongoskin = require('mongoskin');
+let mongoskin = require('mongoskin');
 
 //controller
-var crud = require('./lib/controller/crud/crud');
+let crud = require('./lib/controller/crud/crud');
 
-var DefaultRouter = require('./lib/utils/routers/default');
-var ErrorHandler = require('./lib/utils/errors/util');
-var ErrorStrategy = require('./lib/utils/errors/basic');
+let DefaultRouter = require('./lib/utils/routers/default');
+let ErrorHandler = require('./lib/utils/errors/util');
+let ErrorStrategy = require('./lib/utils/errors/basic');
 
 const MONGO_URL = process.env.MONGO_URL || 'mongodb://@192.168.177.150:27017/test';
 const PORT = 8080;
@@ -19,29 +19,23 @@ const HOST = '0.0.0.0';
 
 console.log("MONGO_URL->", MONGO_URL);
 
-var db = mongoskin.db(MONGO_URL, {
+let db = mongoskin.db(MONGO_URL, {
     safe: true
 });
 
-var errorHandler = ErrorHandler([
+let errorHandler = ErrorHandler([
     ErrorStrategy.basic,
     ErrorStrategy.log
 ]);
 
-var app = express();
-
-app.get('/', (req, res) => {
-    res.send('Services deployed here.');
-});
-
-var companyCollection = db.collection('company')
+let app = express();
+let companyCollection = db.collection('company')
 
 // Append: MIDDLEWARES ===>  [VALIDATE] + [DO NOT EXIST BEFORE]
-var company = require('./lib/routes/company').routing(DefaultRouter(), companyCollection);
+let company = require('./lib/routes/company').routing(DefaultRouter(), companyCollection);
 
+app.get('/', (req, res) => res.send('Services deployed here.'));
 // [VALIDATE] + [DO NOT EXIST BEFORE] + Bussiness logic.
-app.use('/company', require('./lib/routes/crud').routing(company, companyCollection, crud));
-app.use('/account', require('./lib/routes/crud').routing(DefaultRouter(), db.collection('account'), crud))
 app.use('/user',    require('./lib/routes/crud').routing(DefaultRouter(), db.collection('user'), crud));
 
 // error handling middleware.
